55e2840f9a55ad28f81999fa014937ac27591f8b
Kenta Burpee <kentaburpee@gmail.com>
2022-04-28 13:11:45 -0400

    Added repr method and removed numpy methods

--- ultimate_tic_tac_toe_board.py
+++ ultimate_tic_tac_toe_board.py
@@ -9,26 +9,27 @@ from tic_tac_toe_board import TicTacToeBoard
 class UltimateTicTacToeBoard:
 
     def __init__(self):
-        self._boards = []
-        self._ultimate_board_state = []
-        for i in range(0, 9):
-            self._boards.append(TicTacToeBoard())
-            self._ultimate_board_state.append(self._boards[i])
+        self._boards = [[TicTacToeBoard() for i in range(3)] for j in range(3)]
 
     def __repr__(self):
-        ultimate_board_string = ""
-        for board_count in range(0, 3):
-            boards = self._ultimate_board_state[(
-                board_count*3): (3 + board_count*3)]
-            big_row_str = ""
-            for row_count in range(0, 3):
-                row_str = str(boards[:][row_count]) + "\n"
-                big_row_str += row_str
-
-            ultimate_board_string += big_row_str
-        return (ultimate_board_string)
+        divider_string = "+-+-+-++-+-+-++-+-+-+\n"
+        final_string = ""
+        for i in range(3):
+            for j in range(3):
+                final_string += divider_string + "|"
+                for k in range(3):
+                    row = self._boards[i][k].board_state[j]
+                    final_string += "|".join(row)
+                    if k < 2:
+                        final_string += "||"
+                final_string += "|\n"
+            final_string += divider_string
+        return final_string
+                    
 
 
 uboard = UltimateTicTacToeBoard()
-uboard._ultimate_board_state[0].mark(1, 1)
+uboard._boards[1][2].mark(1, 1)
+uboard._boards[1][2].mark(1, 2)
+uboard._boards[1][2].mark(1, 0)
 print(uboard)
