b31b00a2e36e15a7e2681cd251e83ac79394d229
Satchel Sevenau <satchelsevenau@gmail.com>
2022-05-02 16:09:19 -0400

    Registers which section cursor is in.

--- pygame_testing/pygame_Test.py
+++ pygame_testing/pygame_Test.py
@@ -7,9 +7,49 @@ pygame.init()
 # Set up the drawing window
 screen = pygame.display.set_mode([1000, 1000])
 
+# Setting up click function and coordinate system for row and column
+
+# get coordinates of mouse click
+
 # Run until the user asks to quit
 running = True
 while running:
+
+    # to set width of the game window
+    width = 1000
+    # to set height of the game window
+    height = 1000
+
+    x, y = pygame.mouse.get_pos()
+
+    ultimateCol = int
+    ultimateRow = int
+
+    # get column of mouse click (1-3)
+    if(x<width / 3):
+        ultimateCol = 0
+        
+    elif (x<width / 3 * 2):
+        ultimateCol = 1
+        
+    elif(x<width):
+        ultimateCol = 2
+        
+    else:
+        ultimateCol = None
+
+    # get row of mouse click (1-3)
+    if(y<height / 3):
+        ultimateRow = 0
+        
+    elif (y<height / 3 * 2):
+        ultimateRow = 1
+        
+    elif(y<height):
+        ultimateRow = 2
+        
+    else:
+        ultimateRow = None
 
     # Did the user click the window close button?
     for event in pygame.event.get():
@@ -23,9 +63,9 @@ while running:
     screen.fill((255, 255, 255))
 
     # to set width of the game window
-    width = 1000
+    # width = 1000
     # to set height of the game window
-    height = 1000
+    # height = 1000
 
     # to set width of individual games "windoes"
     smallWidth = 300
@@ -64,8 +104,7 @@ while running:
             # drawing horizontal lines
             pygame.draw.line(screen, line_color, (margin + distChangeX, (smallHeight / 3) + distChangeY), (smallWidth + distChangeX, (smallHeight / 3) + distChangeY), 7)
             pygame.draw.line(screen, line_color, (margin + distChangeX, (smallHeight / 3 * 2) + distChangeY), (smallWidth + distChangeX, (smallHeight / 3 * 2) + distChangeY), 7)
-        
-
+    print(f"{ultimateRow},{ultimateCol}")
     # Flip the display
     pygame.display.flip()
 
