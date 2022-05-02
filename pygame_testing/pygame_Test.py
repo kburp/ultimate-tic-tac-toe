8599beba79c5fab57836dfd94df404d78dc85421
Satchel Sevenau <satchelsevenau@gmail.com>
2022-05-02 15:09:05 -0400

    First iteration of game board.

--- pygame_testing/pygame_Test.py
+++ pygame_testing/pygame_Test.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([900, 900])
+screen = pygame.display.set_mode([1000, 1000])
 
 # Run until the user asks to quit
 running = True
@@ -22,24 +22,49 @@ while running:
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(10, 0, 80, 80))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(10, 130, 80, 80))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(10, 260, 80, 80))
+    # to set width of the game window
+    width = 1000
+    # to set height of the game window
+    height = 1000
 
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(140, 0, 80, 80))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(140, 130, 80, 80))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(140, 260, 80, 80))
+    # to set width of individual games "windoes"
+    smallWidth = 300
+    smallHeight = 300
 
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(270, 0, 80, 80))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(270, 130, 80, 80))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(270, 260, 80, 80))
+    # calculate margins for small games
+    margin = ((width / 3) - smallWidth) / 2
+  
+    # color of the straightlines on that 
+    # white game board, dividing board 
+    # into 9 parts
+    line_color = (0, 0, 0)
 
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(120, 0, 10, 360))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(250, 0, 10, 360))
 
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(10, 110, 360, 10))
-    pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(10, 240, 360, 10))
+    # Draw Ultimate board
+        # drawing vertical lines
+    pygame.draw.line(screen, line_color, (width / 3, 0), (width / 3, height), 7)
+    pygame.draw.line(screen, line_color, (width / 3 * 2, 0), (width / 3 * 2, height), 7)
+   
+    # Draw Ultimate board
+        # drawing horizontal lines
+    pygame.draw.line(screen, line_color, (0, height / 3), (width, height / 3), 7)
+    pygame.draw.line(screen, line_color, (0, height / 3 * 2), (width, height / 3 * 2), 7)
+
+    # draw small tic tac toe games
+    for changeInX in range(3):
+        distChangeX = changeInX * (width / 3)
+
+        for changeInY in range(3):
+            distChangeY = changeInY * (width / 3)
+
+            # drawing vertical lines
+            pygame.draw.line(screen, line_color, ((smallWidth / 3) + distChangeX, margin + distChangeY), ((smallWidth / 3) + distChangeX, smallHeight + distChangeY), 7)
+            pygame.draw.line(screen, line_color, ((smallWidth / 3 * 2) + distChangeX, margin + distChangeY), ((smallWidth / 3 * 2) + distChangeX, smallHeight + distChangeY), 7)
+        
+            # drawing horizontal lines
+            pygame.draw.line(screen, line_color, (margin + distChangeX, (smallHeight / 3) + distChangeY), (smallWidth + distChangeX, (smallHeight / 3) + distChangeY), 7)
+            pygame.draw.line(screen, line_color, (margin + distChangeX, (smallHeight / 3 * 2) + distChangeY), (smallWidth + distChangeX, (smallHeight / 3 * 2) + distChangeY), 7)
+        
 
     # Flip the display
     pygame.display.flip()
