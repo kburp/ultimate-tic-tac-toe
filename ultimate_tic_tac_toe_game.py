3acc78a51806205ccec09735a071504d23caea05
Kenta Burpee <kentaburpee@gmail.com>
2022-05-06 22:03:34 -0400

    Added one or two player capability

--- ultimate_tic_tac_toe_game.py
+++ ultimate_tic_tac_toe_game.py
@@ -1,47 +1,74 @@
 """
 Main program to set up and run an ultimate tic-tac-toe game.
 """
-from operator import sub
 
-from numpy import choose
 from ultimate_tic_tac_toe_board import UltimateTicTacToeBoard
 from ultimate_tic_tac_toe_view import TextView
-from ultimate_tic_tac_toe_controller import TextController
+from ultimate_tic_tac_toe_controller import TextComputerController, TextController
 
 
 def main():
     """
     Your docstring goes here.
     """
-    two_players = True
-    if two_players:
-        board = UltimateTicTacToeBoard()
-        textController = TextController(board)
-        textView = TextView(board)
+    print("\nWelcome to ultimate tic tac toe!")
+    print("Enter \'1\' to play against the computer.")
+    print("Enter \'2\' to play a two player game.")
 
-        sub_board_position = textController.choose_board()
+    while True:
+        how_many_players = input("Number of players: ")
+        if how_many_players in ("1", "2"):
+            break
 
-        while True:
-            sub_board = board.boards[sub_board_position[0]][sub_board_position[1]]
-            if not board.check_board_availability(sub_board):
-                sub_board_position = textController.choose_board()
-                continue
+    board = UltimateTicTacToeBoard()
+    textView = TextView(board)
+    player_one = TextController(board)
 
-            sub_board_position = textController.move(sub_board_position)
-
-            if board.check_win():
-                print(f"{board.current_move} won!")
-                break
-            if board.check_tie():
-                print("It's a tie!")
-                break
-
-            board.next_move()
-
-            textView.draw(sub_board_position)
+    if how_many_players == "1":
+        player_two = TextComputerController(board)
     else:
-        # TODO CREATE GAME LOOP FOR AI
-        pass
+        player_two = TextController(board)
+
+    sub_board_position = player_one.choose_board()
+
+    while True:
+        sub_board = board.boards[sub_board_position[0]][sub_board_position[1]]
+        if not board.check_board_availability(sub_board):
+            sub_board_position = player_one.choose_board()
+            continue
+
+        sub_board_position = player_one.move(sub_board_position)
+
+        if board.check_win():
+            print(f"{board.current_move} won!")
+            break
+        if board.check_tie():
+            print("It's a tie!")
+            break
+
+        board.next_move()
+
+        textView.draw(sub_board_position)
+
+        sub_board = board.boards[sub_board_position[0]][sub_board_position[1]]
+        while (not board.check_board_availability(sub_board)) and \
+            isinstance(player_two, TextController):
+            sub_board_position = player_two.choose_board()
+            sub_board = board.boards[sub_board_position[0]][
+                sub_board_position[1]]
+
+        sub_board_position = player_two.move(sub_board_position)
+
+        if board.check_win():
+            print(f"{board.current_move} won!")
+            break
+        if board.check_tie():
+            print("It's a tie!")
+            break
+
+        board.next_move()
+
+        textView.draw(sub_board_position)
 
 
 if __name__ == "__main__":
