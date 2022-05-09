51a5f1e8424daf2ee2ce21df06c77711c0c3942f
Marc Eftimie <marc.eftimie@gmail.com>
2022-05-08 20:52:45 -0400

    Style changes

--- ultimate_tic_tac_toe_controller.py
+++ ultimate_tic_tac_toe_controller.py
@@ -4,65 +4,126 @@ Ultimate tic-tac-toe controller.
 from abc import ABC, abstractmethod
 from random import randint, shuffle
 
-from matplotlib.style import available
-from ultimate_tic_tac_toe_board import UltimateTicTacToeBoard
-
 
 class UltimateTicTacToeController(ABC):
+    """
+    An abstract class that provides decorator properties and abstract mehods
+    for multiple controller sub classes.
 
-    def __init__(self, UltimateTicTacToeBoard):
+    Attributes:
+        _ultimate_tic_tac_toe_board: An UltimateTicTacToeBoard object.
+            board: A decorator property that returns
+        self._ultimate_tic_tac_toe_board.
+    """
+
+    def __init__(self, UltimateTicTacToeBoard):  # pylint: disable=redefined-outer-name
         self._ultimate_tic_tac_toe_board = UltimateTicTacToeBoard
 
     @property
     def board(self):
         """
-        Return the value of _ticTacToeBoard attribute.
+        Return the value of _ultimate_tic_tac_toe_board attribute.
         """
         return self._ultimate_tic_tac_toe_board
 
     @abstractmethod
     def move(self, sub_board_position):
-        pass
+        """
+        An abstract method for getting the players move on a given
+        sub board.
+        """
+
+    @abstractmethod
+    def choose_board(self):
+        """
+        An abstract method for getting the chosen board that the player
+        inputs.
+        """
 
 
 class TextController(UltimateTicTacToeController):
+    """
+    A subclass of TicTacToeController that provides functionallity for
+    player input textually through the terminal.
+    """
 
     def move(self, sub_board_position):
-        sub_board = self._ultimate_tic_tac_toe_board.boards[sub_board_position[0]
-                                                            ][sub_board_position[1]]
+        """
+        Asks for the square that the player wants to chose on a given sub
+        board. Handles incorrect input with exceptions and reasks for valid
+        input until a valid move is inputted.
+
+        Args:
+            sub_board_position: A tuple of two integers which represent the
+                index of the row of the sub board and index of the column of
+                the sub board within the larger ultimate tic-tac-toe-board.
+
+        Returns:
+            A tuple containing two items:
+                A tuple containing two integers representing the index
+                of the row and column of the square chosen on a sub board.
+                The value None, which is a helper value for the main game
+                function.
+        """
+        sub_board = self._ultimate_tic_tac_toe_board.boards[
+            sub_board_position[0]][sub_board_position[1]]
         while True:
             try:
+                # Ask for input
                 mark_position = input("Choose a row and a column (0 - 2): ")
                 mark_position = [int(x) for x in mark_position.split()]
-                if (0 <= mark_position[0] <= 2) and (0 <= mark_position[1] <= 2):
+
+                # Check if input is valid
+                if (0 <= mark_position[0] <= 2) and \
+                        (0 <= mark_position[1] <= 2):
                     sub_board.mark(
-                        mark_position[0], mark_position[1], self._ultimate_tic_tac_toe_board.current_move)
-                    if sub_board.check_win(self._ultimate_tic_tac_toe_board.current_move):
+                        mark_position[0], mark_position[1],
+                        self._ultimate_tic_tac_toe_board.current_move)
+
+                    # Check for a win condition
+                    if sub_board.check_win(self._ultimate_tic_tac_toe_board.
+                                           current_move):
                         self._ultimate_tic_tac_toe_board.mark_win(sub_board)
                     return mark_position, None
-                else:
-                    raise ValueError
-            except:
+                raise ValueError
+            except ValueError:
                 print(f"{mark_position} was not a valid move.")
 
     def choose_board(self):
+        """
+        Asks for the row and column of the sub board that the player wants
+        to select. Called when the first player must choose the starting
+        board and when a player must chose a new valid board.
+
+        Returns:
+            A tuple containing two integers representing the index
+            of the row and column of the sub board within the
+            ultimate tic-tac-toe board.
+        """
         while True:
             try:
+                # Ask for input
                 sub_board_position = input(
                     "Choose the row and column of a board (0 - 2): ")
                 sub_board_position = [int(x)
                                       for x in sub_board_position.split()]
-                sub_board = self._ultimate_tic_tac_toe_board.boards[sub_board_position[0]
-                                                                    ][sub_board_position[1]]
-                if (0 <= sub_board_position[0] <= 2) and (0 <= sub_board_position[1] <= 2) and \
+                sub_board = self._ultimate_tic_tac_toe_board.boards[
+                    sub_board_position[0]][sub_board_position[1]]
+                # Check if input is valid and win condition
+                if (0 <= sub_board_position[0] <= 2) and \
+                    (0 <= sub_board_position[1] <= 2) and \
                         sub_board.win_state == 0:
                     return sub_board_position
                 raise ValueError
-            except:
+            except ValueError:
                 print(f"{sub_board_position} was not a valid move.")
 
 
 class TextComputerController(UltimateTicTacToeController):
+    """
+    A subclass of TicTacToeController that provides functionallity for
+    AI input (works for text and graphical views).
+    """
 
     def move(self, sub_board_position):
         """
@@ -80,8 +141,8 @@ class TextComputerController(UltimateTicTacToeController):
             0]][sub_board_position[1]]
         while sub_board.win_state != 0:
             sub_board_position = (randint(0, 2), randint(0, 2))
-            sub_board = self._ultimate_tic_tac_toe_board.boards[sub_board_position[0]
-                                                                ][sub_board_position[1]]
+            sub_board = self._ultimate_tic_tac_toe_board.boards[
+                sub_board_position[0]][sub_board_position[1]]
         move_square = self.get_best_move(sub_board, self.board.current_move)
         sub_board.mark(move_square[0], move_square[1], self.board.current_move)
         if sub_board.check_win(self.board.current_move):
@@ -93,7 +154,7 @@ class TextComputerController(UltimateTicTacToeController):
         Determines what the most strategic move for the computer is within
         the current TicTacToeBoard instance.
 
-        After determinining which spaces are available, the best move is 
+        After determinining which spaces are available, the best move is
         determined in the following order: If there is only one space left
         on the board, take that space. If the board is empty, chose a random
         space. If there is an open space where the other two spaces in the row,
@@ -131,7 +192,7 @@ class TextComputerController(UltimateTicTacToeController):
                 return space
         return available_spaces[0]
 
-    def check_two_in_a_row(self, board, space, player):
+    def check_two_in_a_row(self, board, space, player):  # pylint: disable=no-self-use
         """
         Checks for a given empty square if the other two squares in the row or
         column (or diagonal) are filled by the same player marker.
@@ -147,7 +208,7 @@ class TextComputerController(UltimateTicTacToeController):
         if board.get_square((row + 1) % 3, col) == \
                 board.get_square((row + 2) % 3, col) == player:
             return True
-        elif board.get_square(row, (col + 1) % 3) == \
+        if board.get_square(row, (col + 1) % 3) == \
                 board.get_square(row, (col + 2) % 3) == player:
             return True
         if row + col == 2 and board.get_square((row + 1) % 3, (col - 1) % 3) \
@@ -156,7 +217,7 @@ class TextComputerController(UltimateTicTacToeController):
         return row == col and board.get_square((row + 1) % 3, (col + 1) % 3) \
             == board.get_square((row + 2) % 3, (col + 2) % 3) == player
 
-    def check_one_and_empty(self, board, space, opponent):
+    def check_one_and_empty(self, board, space, opponent):  # pylint: disable=no-self-use
         """
         Checks for a given empty square if the other two squares in the row or
         column (or diagonal) consist of one current player mark and one empty
@@ -174,18 +235,18 @@ class TextComputerController(UltimateTicTacToeController):
                 board.get_square((row + 1) % 3, col) == opponent or \
                 board.get_square((row + 2) % 3, col) == opponent:
             return False
-        elif board.get_square(row, (col + 1) % 3) == \
+        if board.get_square(row, (col + 1) % 3) == \
                 board.get_square(row, (col + 2) % 3) or \
                 board.get_square(row, (col + 1) % 3) == opponent or \
                 board.get_square(row, (col + 2) % 3) == opponent:
             return False
-        elif row + col == 2 and \
+        if row + col == 2 and \
             (board.get_square((row + 1) % 3, (col - 1) % 3) ==
              board.get_square((row + 2) % 3, (col - 2) % 3) or
              board.get_square((row + 1) % 3, (col - 1) % 3) == opponent or
              board.get_square((row + 2) % 3, (col - 2) % 3) == opponent):
             return False
-        elif row == col and \
+        if row == col and \
             (board.get_square((row + 1) % 3, (col + 1) % 3) ==
              board.get_square((row + 2) % 3, (col + 2) % 3) or
              board.get_square((row + 1) % 3, (col + 1) % 3) == opponent or
@@ -195,31 +256,68 @@ class TextComputerController(UltimateTicTacToeController):
 
 
 class GraphicalController(UltimateTicTacToeController):
+    """
+    A subclass of TicTacToeController that provides functionallity for
+    player input graphically through a pygame window.
+    """
 
-    def move(self, sub_board_position, sub_board_square_position):
-        sub_board = self._ultimate_tic_tac_toe_board.boards[sub_board_position[0]
-                                                            ][sub_board_position[1]]
+    def move(self, sub_board_position, sub_board_square_position):  # pylint: disable=arguments-differ
+        """
+        Asks for the square that the player wants to chose on a given sub
+        board. Handles incorrect input with exceptions and reasks for valid
+        input until a valid move is inputted.
+
+        Args:
+            sub_board_position: A tuple of two integers which represent the
+                index of the row of the sub board and index of the column of
+                the sub board within the larger ultimate tic-tac-toe-board.
+            sub_board_square_position: A tuple of two integers which represent
+                the row and column of the selected square within the sub board.
+
+        Returns:
+            A tuple containing two items:
+                A tuple containing two integers representing the index
+                of the row and column of the square chosen on a sub board.
+                The value None, which is a helper value for the main game
+                function.
+        """
+        sub_board = self._ultimate_tic_tac_toe_board.boards[
+            sub_board_position[0]][sub_board_position[1]]
         try:
-            if (0 <= sub_board_square_position[0] <= 2) and (0 <= sub_board_square_position[1] <= 2):
+            if (0 <= sub_board_square_position[0] <= 2) and \
+                    (0 <= sub_board_square_position[1] <= 2):
                 sub_board.mark(
-                    sub_board_square_position[0], sub_board_square_position[1], self._ultimate_tic_tac_toe_board.current_move)
-                if sub_board.check_win(self._ultimate_tic_tac_toe_board.current_move):
+                    sub_board_square_position[0],
+                    sub_board_square_position[1],
+                    self._ultimate_tic_tac_toe_board.current_move)
+                if sub_board.check_win(
+                        self._ultimate_tic_tac_toe_board.current_move):
                     self._ultimate_tic_tac_toe_board.mark_win(sub_board)
                 return sub_board_square_position
-            else:
-                raise ValueError
-        except:
-            print(f"{sub_board_square_position} was not a valid move.")
             raise ValueError
+        except ValueError as v_err:
+            print(f"{sub_board_square_position} was not a valid move.")
+            raise ValueError from v_err
 
-    def choose_board(self, sub_board_position):
+    def choose_board(self, sub_board_position):  # pylint: disable=arguments-differ
+        """
+        Asks for the row and column of the sub board that the player wants
+        to select. Called when the first player must choose the starting
+        board and when a player must chose a new valid board.
+
+        Returns:
+            A tuple containing two integers representing the index
+            of the row and column of the sub board within the
+            ultimate tic-tac-toe board.
+        """
         try:
-            sub_board = self._ultimate_tic_tac_toe_board.boards[sub_board_position[0]
-                                                                ][sub_board_position[1]]
-            if (0 <= sub_board_position[0] <= 2) and (0 <= sub_board_position[1] <= 2) and \
+            sub_board = self._ultimate_tic_tac_toe_board.boards[
+                sub_board_position[0]][sub_board_position[1]]
+            if (0 <= sub_board_position[0] <= 2) and \
+                (0 <= sub_board_position[1] <= 2) and \
                     sub_board.win_state == 0:
                 return sub_board_position
             raise ValueError
-        except:
+        except ValueError as v_err:
             print(f"{sub_board_position} was not a valid move.")
-            raise ValueError
+            raise ValueError from v_err
